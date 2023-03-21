4c34ecf872ae63c6866cf906b129420aa33737ff
GameJarne <katpoes373.devolder@gmail.com>
2023-03-21 20:48:18 +0100

    player movement and island models

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private const string ISWALKING_ANIM_PARAMETER = "isWalking";
+
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 2f;
+    [SerializeField] private Transform gfxTransform;
+    [SerializeField] private Animator animator;
+
+    private Rigidbody rb;
+
+    private Vector2 movementInput;
+    private bool isMoving;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (movementInput.x != 0 || movementInput.y != 0)
+        {
+            isMoving = true;
+        } else
+        {
+            isMoving = false;
+        }
+
+        UpdateAnimations();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDir = new Vector3(movementInput.x, 0f, movementInput.y);
+
+        Vector3 moveVector = moveDir * speed * 10f * Time.deltaTime;
+        rb.velocity = new Vector3(moveVector.x, rb.velocity.y, moveVector.z);
+
+        float rotateSpeed = 10f;
+        gfxTransform.forward = Vector3.Slerp(gfxTransform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    private void UpdateAnimations()
+    {
+        if (isMoving)
+        {
+            animator.SetBool(ISWALKING_ANIM_PARAMETER, true);
+        }
+        else
+        {
+            animator.SetBool(ISWALKING_ANIM_PARAMETER, false);
+        }
+    }
+}
