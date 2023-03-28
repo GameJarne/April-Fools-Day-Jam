8802228f826ac370d5cff47bdecc12940cac1cc8
GameJarne <katpoes373.devolder@gmail.com>
2023-03-28 20:01:54 +0200

    added functionality to the main menu
    settings do not work yet

---
+++ Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainPanel;
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject levelsPanel;
+
+    private void Start()
+    {
+        mainPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+        levelsPanel.SetActive(false);
+    }
+
+    public void OnPlayButton()
+    {
+        levelsPanel.SetActive(true);
+    }
+    public void OnExitLevelManagerButton()
+    {
+        levelsPanel.SetActive(false);
+    }
+
+    public void OnSettingsButton()
+    {
+        settingsPanel.SetActive(true);
+    }
+    public void OnExitSettingsButton()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+    }
+}
