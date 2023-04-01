7c8b4d4461c37735f072ad6a3937632ee1995c26
GameJarne <katpoes373.devolder@gmail.com>
2023-04-01 13:49:19 +0200

    player will now also ragdoll when dying to fall in void

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -7,17 +7,22 @@ using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour
 {
     private const string INSTANT_DEATH_TAG = "Instant Death";
+    private const string LIVING_ASTEROID_TAG = "Living Asteroid";
 
     [SerializeField] private float restartTime = 3f;
+    [SerializeField] private GameObject playerGraphics;
+    [SerializeField] private GameObject playerRagdollPrefab;
 
     public UnityEvent onDeath;
 
+    private bool isDead = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("entered a trigger");
-        if (other.gameObject.CompareTag(INSTANT_DEATH_TAG))
+        if (other.gameObject.CompareTag(INSTANT_DEATH_TAG) && !isDead)
         {
-            Debug.Log("Player has died.");
+            isDead = true;
+            SetRagdoll();
             onDeath?.Invoke();
             StartCoroutine(RestartAfterTime());
         }
@@ -25,7 +30,22 @@ public class PlayerHealth : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.CompareTag(LIVING_ASTEROID_TAG) && !isDead)
+        {
+            isDead = true;
+            SetRagdoll();
+            onDeath?.Invoke();
+            StartCoroutine(RestartAfterTime());
+        }
+    }
+
+    private void SetRagdoll()
+    {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        playerGraphics.SetActive(false);
+        GameObject ragdoll = Instantiate(playerRagdollPrefab, this.transform);
+        ragdoll.transform.position = transform.position;
+        ragdoll.transform.localRotation = playerGraphics.transform.localRotation;
     }
 
     private IEnumerator RestartAfterTime()
